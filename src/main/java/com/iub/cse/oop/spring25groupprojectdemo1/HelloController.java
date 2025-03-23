7c9a757252a041d23b743bb5b866154ea05c6793
Asif Mahmood <asif.mahmood@iub.edu.bd>
2025-03-23 09:44:03 +0600

    Added scene switching

--- src/main/java/com/iub/cse/oop/spring25groupprojectdemo1/HelloController.java
+++ src/main/java/com/iub/cse/oop/spring25groupprojectdemo1/HelloController.java
@@ -1,14 +1,23 @@
 package com.iub.cse.oop.spring25groupprojectdemo1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("asif/user3-dashboard.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) welcomeText.getScene().getWindow();
+        stage.setScene(scene);
     }
 }
--- src/main/java/com/iub/cse/oop/spring25groupprojectdemo1/asif/User3DashboardController.java
+++ src/main/java/com/iub/cse/oop/spring25groupprojectdemo1/asif/User3DashboardController.java
@@ -1,4 +1,23 @@
 package com.iub.cse.oop.spring25groupprojectdemo1.asif;
 
+import com.iub.cse.oop.spring25groupprojectdemo1.HelloApplication;
+import com.iub.cse.oop.spring25groupprojectdemo1.HelloController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class User3DashboardController {
+    @FXML
+    public void switchScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(HelloController.class.getResource("mahmood/user7-dashboard.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+    }
 }
